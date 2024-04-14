a69870423a1948c99cf7a25334077627131dddb6
Immaculatewangari <immaculate.wangari@student.moringaschool.com>
2024-04-14 23:45:33 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const drivers = [
+    { name: "Bushra", hometown: "New York" },
+    { name: "Anes", hometown: "Moscow" },
+    { name: "Shiraz", hometown: "Berlin" },
+    { name: "Wssim", hometown: "Paris" },
+  ];
+  
+  function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
